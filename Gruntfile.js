064b7e8ac7b9fe723a21517a074dbd0ca91731e9
lsphillips <lsphillips.mail@gmail.com>
2014-09-14 11:08:12 +0100

    Minor tweaks
    Converted all spaces to tabs in the configuration files.
    Updated grunt tasks to latest versions.
    Cleaned up the .jshintrc file by removing comments which isn’t valid
    JSON.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,86 @@
 module.exports = function (grunt)
 {
-    // Dependencies
-    // ----------------------------------------------------
-    
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+	// Dependencies
+	// ----------------------------------------------------
+	
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
 
-    // Configuration
-    // ----------------------------------------------------
-    
-    grunt.initConfig(
-    {
-        // Cleaning
-        // ------------------------------------------------
+	// Configuration
+	// ----------------------------------------------------
+	
+	grunt.initConfig(
+	{
+		// Cleaning
+		// ------------------------------------------------
 
-        clean :
-        {
-            src : ['dist']
-        },
+		clean :
+		{
+			src : ['dist']
+		},
 		
-        
-        // Tests
-        // ------------------------------------------------
+		
+		// Tests
+		// ------------------------------------------------
 
-        nodeunit :
-        {
-            options :
-            {
-                reporter : 'verbose'
-            },
+		nodeunit :
+		{
+			options :
+			{
+				reporter : 'verbose'
+			},
 
-            all : ['test/Clazzy.js']
-        },
-
-
-        // Compression
-        // ------------------------------------------------
-
-        uglify :
-        {
-            build :
-            {
-                src  : 'src/Clazzy.js',
-                dest : 'dist/Clazzy.js'
-            },
-
-            options : { report : 'gzip' }
-        },
+			all : ['test/Clazzy.js']
+		},
 
 
-        // Code Quality
-        // ------------------------------------------------
-        
-        jshint :
-        {
-            options :
-            {
-                jshintrc : '.jshintrc'
-            },
-            
-            src : ['src/Clazzy.js', 'test/Clazzy.js']
-        }
-    });
-    
-    
-    // Task: `default`
-    // ----------------------------------------------------
-    
-    grunt.registerTask('default', ['jshint', 'nodeunit', 'clean', 'uglify']);
-    
-    // Task: `build`
-    // ----------------------------------------------------
-    
-    grunt.registerTask('build', ['jshint', 'nodeunit', 'clean', 'uglify']);
-    
-    // Task: `test`
-    // ----------------------------------------------------
-    
-    grunt.registerTask('test', ['jshint', 'nodeunit']);
+		// Compression
+		// ------------------------------------------------
+
+		uglify :
+		{
+			build :
+			{
+				src  : 'src/Clazzy.js',
+				dest : 'dist/Clazzy.js'
+			},
+
+			options : { report : 'gzip' }
+		},
+
+
+		// Code Quality
+		// ------------------------------------------------
+		
+		jshint :
+		{
+			options :
+			{
+				jshintrc : '.jshintrc'
+			},
+			
+			src : ['src/Clazzy.js', 'test/Clazzy.js']
+		}
+	});
+	
+	
+	// Task: `default`
+	// ----------------------------------------------------
+	
+	grunt.registerTask('default', ['jshint', 'nodeunit', 'clean', 'uglify']);
+	
+
+	// Task: `build`
+	// ----------------------------------------------------
+	
+	grunt.registerTask('build', ['jshint', 'nodeunit', 'clean', 'uglify']);
+	
+	
+	// Task: `test`
+	// ----------------------------------------------------
+	
+	grunt.registerTask('test', ['jshint', 'nodeunit']);
 };
